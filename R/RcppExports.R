879594194d98a1bc55e7185c8b2faef414793f95
Nathan Wycoff <nathanbrwycoff@gmail.com>
2018-08-31 09:21:11 -0400

    Example executes via example function now

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -14,6 +14,27 @@
 #' @param t_eps A scalar double, the step size for numerical integration.
 #' @param t_steps A scalar integer, the number of finite difference iterations.
 #' @return A list of lists of numeric vectors. The first entry will be Fin, and so on.
+#' @examples
+#' t_eps <- 0.01
+#' t_end <- 10
+#' ts <- seq(0, t_end, by = t_eps)
+#' t_steps <- length(ts)
+#' n_in <- 2
+#' n_out <- 1
+#' n_h <- c(1)
+#' layers <- 2 + length(n_h)
+#' #Ws <- list(matrix(c(3.5), ncol = 1), matrix(c(3), ncol = 1))
+#' Fin <- list(seq(0,10, by = 1), seq(0,10, by = 1))
+#' # Generate random wieghts
+#' set.seed(123)
+#' sizes <- c(n_in, n_h, n_out)
+#' Ws <- lapply(1:(length(sizes)-1), function(i) 
+#'              matrix(rnorm(sizes[i]*sizes[i+1], 3), nrow = sizes[i], ncol = sizes[i+1]))
+#' l_h <- length(n_h)
+#' a <- goc(n_in, l_h, n_h, n_out, Ws, Fin, t_eps, t_steps) 
+#' cat('Firing Times by layer-neuron:\n')
+#' print(a)
+#'
 #' @export
 goc <- function(n_in, l_h, n_h, n_out, Ws, Fin, t_eps, t_steps) {
     .Call(`_snnLearn_goc`, n_in, l_h, n_h, n_out, Ws, Fin, t_eps, t_steps)
