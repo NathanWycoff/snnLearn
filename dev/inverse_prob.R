39e937f49a5aa2305792d12da37fdaba0c69e2c4
Nathan Wycoff <nathanbrwycoff@gmail.com>
2018-12-02 21:57:12 -0500

    General case may be working?

--- dev/inverse_prob.R
+++ dev/inverse_prob.R
@@ -9,7 +9,7 @@ postkern <- function(dt) as.numeric(dt>=0) * exp(-dt/tau)# Simply the kernel its
 dpostkernd <- function(dt) as.numeric(dt>=0) * (-1)/tau * exp(-dt/tau)# Derivative of the kernel 
 iprekern <- function(dt) as.numeric(dt>=0) * -v_thresh# Integrated kernel
 
-t_eps <- 0.01
+t_eps <- 0.1
 t_end <- 5
 ts <- seq(0, t_end, by = t_eps)
 t_steps <- length(ts)
@@ -19,7 +19,7 @@ v_reset <- 0
 
 n_in <- 3
 n_out <- 2
-n_h <- c(2)
+n_h <- c(3, 2)
 net_shape <- c(n_in, n_h, n_out)
 n_layers <- 2 + length(n_h)
 #Ws <- list(matrix(c(3.5), ncol = 1), matrix(c(3), ncol = 1))
@@ -40,10 +40,13 @@ Fin <- lapply(1:n_in, function(n) runif(sample(max_input_fire, 1), 0, t_end))
 ret <- srm0_R(Ws, net_shape, Fin, t_steps, t_eps)
 
 #TODO: generalize t_desired to multiple per point
-t_desired <- c(1.69, 4.20)
+#TODO: can't yet handle the case where there are fewer observed firings than desired at the beginning
+#t_desired <- list(c(1,2,3), c(1.5, 3), c(4.2)) #A list of numeric vectors as long as the output layer, each giving a desired firing time.
+t_desired <- list(c(1), c(4)) #A list of numeric vectors as long as the output layer, each giving a desired firing time.
+t_ns <- sapply(t_desired, length)
 
 iters <- 1000
-learn_rate <- 0.01
+learn_rate <- 1
 last_Ws <- Ws
 grad_norm <- Inf
 
@@ -59,12 +62,15 @@ for (iter in 1:iters) {
     #target_neuron <- 1
     #ta <- Fout[[target_neuron]][1]
     #tai <- which(abs(ts-ta) < t_eps/2)
-    t_actuals <- sapply(Fout, function(fs) fs[1])
-    print(paste("Iter:", iter, "has firing times", paste(t_actuals, collapse = ", "), "with norm", grad_norm))
+    #t_actuals <- sapply(Fout, function(fs) fs[1])
+    t_actuals <- lapply(1:n_out, function(n) Fout[[n]][1:t_ns[n]])
+    err <- sum(sapply(1:n_out, function(n) sum((t_actuals[[n]] - t_desired[[n]])^2)))
+
+    print(paste("Iter:", iter, "has firing times", paste(t_actuals, collapse = ", "), "with gradient norm", grad_norm, "|| Error:", err))
 
     #TODO: Modify next line for firings per output
-    if (any(sapply(Fout, length) == 0)) {
-        print("No firing events detected, halving learning rate and disregarding last iter...")
+    if (any(sapply(Fout, length) - t_ns < 0)) {
+        print("Insufficient firing events detected, halving learning rate and disregarding last iter...")
         learn_rate <- learn_rate / 2
         print(paste("New learn rate:", learn_rate))
         Ws <- last_Ws
@@ -72,51 +78,107 @@ for (iter in 1:iters) {
     }
 
     # Output delta
-    d_out <- rep(NA, n_out)
+    #d_out <- rep(NA, n_out)
+    d_out <- lapply(1:n_out, function(n) rep(NA, t_ns[n]))
     for (n in 1:n_out) {
         #TODO: one non assumption: modify td & ta
         #d_out[n] <- -(ta - td) / t(Ws[[length(n_h)+1]]) %*% GAMMAd[[length(n_h)+1]][,tai]
 
         # TODO: The other 1 says "only look at the first firing event"
-        d_out[n] <- -(t_actuals[n] - td[n]) / t(Ws[[length(n_h)+1]][,n]) %*% GAMMAd[[n]][[1]][[length(n_h)+1]]
+        for (fi in 1:t_ns[n]) {
+            dt <-  t_desired[[n]][fi] - t_actuals[[n]][fi]
+            d_out[[n]][fi] <- dt / (t(Ws[[length(n_h)+1]][,n]) %*% GAMMAd[[n]][[fi]][[length(n_h)+1]])
+        }
     }
 
     # Hidden Delta
-    d_h <- lapply(n_h, function(h) matrix(NA, nrow = h, ncol = n_out))
-    for (l in length(n_h):1) {
-        for (neur in 1:n_h[l]) {
-            #TODO: d_out not good for more than 1 hidden layer
-            #d_h[[l]][neur] <-  d_out * (GAMMAd[[l+1]][neur,tai] * sum(Ws[[l+1]][neur,]))  /
-            #    t(Ws[[l]][,neur]) %*% GAMMAd[[l]][,tai]
-            # TODO: The other 1 says "only look at the first firing event"
-            d_h[[l]][neur,] <-  d_out * (GAMMAd[[1]][[1]][[l+1]][neur] * sum(Ws[[l+1]][neur,]))  /
-                t(Ws[[l]][,neur]) %*% GAMMAd[[1]][[1]][[l]]
+    #d_h <- lapply(n_h, function(h) matrix(NA, nrow = h, ncol = n_out))
+    # Still assumes 1 hidden layer
+    d_h <- lapply(1:n_out, function(on) lapply(1:t_ns[on], function(f) lapply(n_h, function(h) rep(NA, h))))
+    #for (l in length(n_h):1) {
+    #    for (neur in 1:n_h[l]) {
+    #        #TODO: d_out not good for more than 1 hidden layer
+    #        #d_h[[l]][neur] <-  d_out * (GAMMAd[[l+1]][neur,tai] * sum(Ws[[l+1]][neur,]))  /
+    #        #    t(Ws[[l]][,neur]) %*% GAMMAd[[l]][,tai]
+    #        # TODO: The other 1 says "only look at the first firing event"
+    #        #d_h[[l]][neur,] <-  d_out * (GAMMAd[[1]][[1]][[l+1]][neur] * sum(Ws[[l+1]][neur,]))  /
+    #        #    t(Ws[[l]][,neur]) %*% GAMMAd[[1]][[1]][[l]]
+    #    }
+    #}
+    for (n in 1:n_out) {
+        for (fi in 1:t_ns[n]) {
+            for (l in length(n_h):1) {
+                for (h in 1:net_shape[l+1]) {
+                    #d_h[[n]][[fi]][[l]][h] <- d_out[[n]][fi] * (GAMMAd[[n]][[fi]][[l+1]] %*% Ws[[l+1]][h,])  /
+                    #    t(Ws[[l]][,h]) %*% GAMMAd[[n]][[fi]][[l]]
+                    # Note sure out of these two:
+                    #num <- d_out[[n]][[fi]] * Ws[[l+1]][h,n]
+                    if (l == length(n_h)) {
+                        num <- d_out[[n]][[fi]] * Ws[[l+1]][h,n]
+                    } else {
+                        #num <- d_h[[n]][[fi]][[l+1]] %*% (Ws[[l]][h,] * GAMMAd[[n]][[fi]][[l+1]])
+                        num <- (d_h[[n]][[fi]][[l+1]] %*% Ws[[l+1]][h,]) * GAMMAd[[n]][[fi]][[l+1]][h]
+                    }
+                    denom <- t(Ws[[l]][,h]) %*% GAMMAd[[n]][[fi]][[l]]
+                    d_h[[n]][[fi]][[l]][h] <- num / denom
+                }
+            }
         }
     }
-    delta <- d_h
-    delta[[length(delta)+1]] <- matrix(d_out, nrow = 1)
+
+    # Delta is organized as such
+    # delta[on] gives the delta related to firing events for output neuron "on"
+    # delta[on][fi] gives the delta related to the fi'th firing event for output neuron "on"
+    # delta[on][fi][l] gives the delta's in layer l (INDEXED IN REVERSE) related to the fi'th firing event for output neuron "on"
+    # delta[on][fi][l][h] gives the deltas in neuron h of layer l (INDEXED IN REVERSE) related to the fi'th firing event for output neuron "on"
+    #delta <- d_h
+    #for (n in 1:n_out) {
+    #    for (fi in 1:t_ns[n]) {
+    #        delta[[n]][[fi]][[length(n_h)+1]] <- d_out[[n]][[fi]]
+    #    }
+
+    #}
 
     # Calculate weight updates, and apply them
     last_Ws <- Ws
     grad_norm <- 0
     for (wi in 1:length(Ws)) {
         #Wd <- -t(delta[[wi]]) %x% GAMMA[[wi]][,tai]
-        # TODO: the 1 in the next line means first firing event, and should be changed in the future.
-        # Each columns has its own gradient for output layers, summed for the others
+
+        # Each columns has its own gradient for output layers, summed for the others, so we need the input statement
         # since we're averaging gradients for hidden layers, we should do the same for the output weights.
         if (wi == length(Ws)) {
+            next
             for (on in 1:n_out) {
-                Wd <- -t(delta[[wi]][,on]) %x% GAMMA[[on]][[1]][[wi]] / n_out
+                Wd <- 0
+                for (fi in 1:t_ns[on]) {
+                    #for (npre in 1:net_shape[wi]) {
+                    #    for (npost in 1:net_shape[wi+1]) {
+                    #        Wd[npre, npost] <- Wd[npre, npost] + delta[[n]][[fi]][[wi]][[npost]] * GAMMA[[n]][[fi]][[wi]][[npre]]
+                    #    }
+                    #}
+                    # These are equivalent
+                    Wd <- Wd + t(d_out[[on]][[fi]]) %x% GAMMA[[on]][[fi]][[wi]] 
+                }
+                Wd <- Wd / sum(t_ns)
                 grad_norm <- grad_norm + sum(Wd^2)
                 Ws[[wi]][,on] <- Ws[[wi]][,on] - learn_rate * Wd
             }
         } else {
             Wd <- 0
             for (on in 1:n_out) {
-                Wd <- Wd + -t(delta[[wi]][,on]) %x% GAMMA[[on]][[1]][[wi]]
+                for (fi in 1:t_ns[on]) {
+                    #for (npre in 1:net_shape[wi]) {
+                    #    for (npost in 1:net_shape[wi+1]) {
+                    #        Wd[npre, npost] <- delta[[n]][[fi]][[wi]][[npost]] * GAMMA[[n]][[fi]][[wi]][[npre]]
+                    #    }
+                    #}
+                    # These are equivalent
+                    Wd <- Wd + t(d_h[[on]][[fi]][[wi]]) %x% GAMMA[[on]][[fi]][[wi]]
+                }
             }
             # Scale by number of output neurons to keep gradient size similar
-            Wd <- Wd / n_out
+            Wd <- Wd / sum(t_ns)
             grad_norm <- grad_norm + sum(Wd^2)
             Ws[[wi]] <- Ws[[wi]] - learn_rate * Wd
         }
